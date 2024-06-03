24f504296c797d70e7c4a04b104ea46f4cbe877d
KevinMoyolema <kevinmoyolema13@gmail.com>
2024-06-03 13:09:19 -0500

    Add files via upload

--- consultas.sql
+++ consultas.sql
@@ -1,10 +1,11 @@
 SELECT
     alumno_ambato.codigo AS "Codigo Alumno Ambato",
     alumno_ambato.*,
+    alumno_quito.codigo AS "Codigo Alumno Quito",
+    alumno_quito.*,
     alumno_guayaquil.codigo AS "Codigo Alumno Guayaquil",
     alumno_guayaquil.*
 FROM
-    `alumno_ambato`
+    alumno_ambato
+INNER JOIN alumno_quito ON alumno_ambato.codigo = alumno_quito.codigo
 INNER JOIN alumno_guayaquil ON alumno_ambato.codigo = alumno_guayaquil.codigo;
--- Esta consulta une los datos de las tablas 'alumno_ambato' y 'alumno_guayaquil' basándose en el campo 'codigo', 
--- mostrando todos los campos de ambas tablas con alias para los códigos de cada tabla.
